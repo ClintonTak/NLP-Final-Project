9379ecaa503a8d664d011a832ab4e1b28fcd78f4
Isaiah Rawlinson <ijg.rawlinson@gmail.com>
2018-04-30 19:42:49 -0400

    added gpu support to cut down training time

---
+++ TFLearn/clean_logs.py
@@ -0,0 +1,23 @@
+# title           : clean_logs.py
+# description     : Scrape logs down to final result of each epoch
+# author          : Becker, Brett, Tak, and Rawlinson
+# date            : Monday, 30 April 2018.
+# python_version  : 3.6.4
+# ==================================================
+
+import sys
+
+
+def parse(filename):
+    log_file = open(filename).readlines()
+    lines = []
+
+    for i in range(len(log_file)):
+        if 'val_loss' in log_file[i]:
+            line = log_file[i-2] + log_file[i]
+            lines.append(line.strip())
+
+    sys.stdout = open(filename, 'w')
+    for line in lines:
+        print(line, '\n\n', '-'*80, '\n')
+    sys.stdout = sys.__stdout__
--- TFLearn/cnn.py
+++ TFLearn/cnn.py
@@ -6,10 +6,25 @@
 # ==================================================
 
 from __future__ import division, print_function, absolute_import
+import sys
+import clean_logs
+import read_data
 
+import tensorflow as tf
 import tflearn
 from tflearn.data_utils import to_categorical, pad_sequences
-import read_data
+
+from datetime import datetime
+
+# Set up command line arguments:
+args = sys.argv[1:]
+gpu_mode = '-gpu' in args
+mac_os = '-mac' in args
+
+
+startTime = datetime.now()
+
+logfile = 'log.txt'
 
 """
 TF in this example expects (list, int) pairs so I changed the one-hot
@@ -33,18 +48,46 @@ we have classes for each language, so 6
 trainY = to_categorical(trainY, nb_classes=6)
 testY = to_categorical(testY, nb_classes=6)
 
+
 # Network building
-net = tflearn.input_data([None, length])
-net = tflearn.embedding(net, input_dim=10000, output_dim=128)
-net = tflearn.lstm(net, 128, dropout=0.8)
-# Same thing here as with nb_classes, need to change to 6
-net = tflearn.fully_connected(net, 6, activation='softmax')
-net = tflearn.regression(net, optimizer='adam', learning_rate=0.001,
-                         loss='categorical_crossentropy')
+def build_network():
+    net = tflearn.input_data([None, length])
+    net = tflearn.embedding(net, input_dim=10000, output_dim=128)
+    net = tflearn.lstm(net, 128, dropout=0.8)
+    # Same thing here as with nb_classes, need to change to 6
+    net = tflearn.fully_connected(net, 6, activation='softmax')
+    net = tflearn.regression(net, optimizer='adam', learning_rate=0.001,
+                             loss='categorical_crossentropy')
+    return net
+
 
 # Training
-model = tflearn.DNN(net, tensorboard_verbose=0)
-model.fit(trainX, trainY,
-          validation_set=(testX, testY),
-          show_metric=True,
-          batch_size=32)
+def train(net):
+    model = tflearn.DNN(net, tensorboard_verbose=0)
+    model.fit(trainX, trainY,
+              validation_set=(testX, testY),
+              show_metric=True,
+              batch_size=32,
+              snapshot_step=None)
+
+
+if gpu_mode:
+    tflearn.init_graph(num_cores=4, gpu_memory_fraction=0.5)
+    with tf.device('/device:GPU:0'):
+        net = build_network()
+        # Redirect logs to a file
+        if not mac_os:
+            sys.stdout = open(logfile, 'w')
+        train(net)
+    sys.stdout = sys.__stdout__
+else:
+    net = build_network()
+    if not mac_os:
+        sys.stdout = open(logfile, 'w')
+    train(net)
+    sys.stdout = sys.__stdout__
+
+if not mac_os:
+    clean_logs.parse(logfile)
+
+print('Process finished in', datetime.now() - startTime)
